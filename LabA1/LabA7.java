239a660498e187a4b6fdc659665a46c4237ab1f6
Jonathan <yekeb00f@gmail.com>
2018-09-21 02:11:33 -0400

    labB

--- LabA1/LabA7.java
+++ LabA1/LabA7.java
@@ -11,15 +11,15 @@ public class LabA7 {
         
         int y = x & mask; // set everything but 1 & 1 to 0
         
-        y = y >> 10; // make 1 the lsb
+        y = y >> 10; // make bit #10 the lsb
         
         int z = x & secondmask;  // set everything but 1 & 1 to 0
         
-        z = z >> 20;  // make 1 the lsb
+        z = z >> 20;  // make bit #20 the lsb
 
         if(z != y){ // if bit #10 and bit #20 are not the same
-        	x = mask ^ x;  //use 1st mask on x to swap value
-        	x = secondmask ^ x; // use 2nd mask on x to swap value
+        	x = mask ^ x;  //use 1st mask on x to toggle value of 10th bit
+        	x = secondmask ^ x; // use 2nd mask on x to toggle value 20th bit
         }
         
         System.out.println(Integer.toBinaryString(x)+ "  (swapped");
